1309f5ec507920c9acf9e06b3269886eaf71a1a8
TazmeenM <tazmeen.mohsin@gmail.com>
2025-08-25 13:01:10 -0400

    Added Collisions and Collision Code
    Added code to make the robot change directions when it touches one of the boxes.

---
+++ scripts/pickup_box.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var robot_1: CharacterBody2D = $"../Background/Robot1"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Robot:
+		body.yDirection = body.yDirection*-1
+		print(body.yDirection)
--- scripts/robot_1.gd
+++ scripts/robot_1.gd
@@ -1,8 +1,9 @@
-extends CharacterBody2D
+class_name Robot extends CharacterBody2D
 
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var yDirection := 1
 
 
 func _physics_process(delta: float) -> void:
@@ -11,15 +12,12 @@ func _physics_process(delta: float) -> void:
 		#velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := 1
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	velocity.y = yDirection * SPEED
 
 	move_and_slide()
