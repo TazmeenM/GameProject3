d9cebc59633c54248b605567cb580f22d92a0de5
TazmeenM <tazmeen.mohsin@gmail.com>
2025-08-27 09:11:21 -0400

    Added Apple Collection
    Added a way to collect the apple once it's been fully rendered.

--- scripts/appleScript.gd
+++ scripts/appleScript.gd
@@ -1,7 +1,11 @@
 extends Area2D
+
+@onready var resources: Resources = $"../../Resources"
+
 var percentageOfApple: float = 0
 var amountOfResources = 0
 var capacity: float = 10
+var numberOfApplesAtOnce = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +15,14 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_apple_button_pressed() -> void:
+	if percentageOfApple == 1:
+		pickUp()
+		
+
+func pickUp() -> void:
+	percentageOfApple = 0
+	amountOfResources = 0
+	resources.addApples(numberOfApplesAtOnce)
--- scripts/resources.gd
+++ scripts/resources.gd
@@ -1,6 +1,7 @@
 class_name Resources extends Node2D
 
 static var numberOfResources = 0
+static var numberOfApples = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,3 +14,7 @@ func _process(delta: float) -> void:
 
 static func addResources(numberOfResourcesAdded: int) -> void:
 	numberOfResources += numberOfResourcesAdded
+
+static func addApples(numberOfApplesAdded: int) -> void:
+	numberOfApples += numberOfApplesAdded
+	print("Number Of Apples: " + str(numberOfApples))
