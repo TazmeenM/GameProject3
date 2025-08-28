163f280127e5d03ef2373a76f4f94957487debac
TazmeenM <tazmeen.mohsin@gmail.com>
2025-08-28 18:02:27 -0400

    Started Adding Feeding
    Started adding a way to feed the astronaut to keep the health bar high. Also optimized some code for the resources.

--- scripts/astronaut.gd
+++ scripts/astronaut.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 @onready var health_bar: ProgressBar = $HealthBar
+@onready var apple_button: Button = $AppleButton
+@onready var orange_button: Button = $OrangeButton
+@onready var pear_button: Button = $PearButton
 
 var maximumHealth = 100
 var health = 100
@@ -13,11 +16,40 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	
+	if apple_button:
+		if Resources.numberOfApples > 0:
+			apple_button.disabled = false
+		elif Resources.numberOfApples <= 0:
+			apple_button.disabled = true
+	if orange_button:
+		if Resources.numberOfOranges > 0:
+			orange_button.disabled = false
+		elif Resources.numberOfOranges <= 0:
+			orange_button.disabled = true
+	if pear_button:
+		if Resources.numberOfPears > 0:
+			pear_button.disabled = false
+		elif Resources.numberOfPears <= 0:
+			pear_button.disabled = true
 
 func decreaseHealth(healthDecreased: int) -> void:
 	health -= healthDecreased
 
 func increaseHealth(healthIncreased: int) -> void:
 	health += healthIncreased
+
+
+func _on_apple_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_orange_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_pear_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_scene_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
--- scripts/resources.gd
+++ scripts/resources.gd
@@ -4,9 +4,17 @@ static var numberOfResources = 0
 static var numberOfFruits = 0
 static var numberOfApples = 0
 static var numberOfOranges = 0
+static var numberOfPears = 0
 static var money = 0
+
+static var fruitValues = {
+	"apple": 10,
+	"orange": 15,
+	"pear": 25
+}
 static var appleWorth = 10
 static var orangeWorth = 15
+static var pearWorth = 25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,6 +34,25 @@ static func addFruits(numberOfFruitsAdded: int, fruitName: String) -> void:
 	if fruitName == "apple":
 		numberOfApples += numberOfFruitsAdded
 		print("Number of Apples: " + str(numberOfApples))
-	if fruitName == "orange":
+	elif fruitName == "orange":
 		numberOfOranges += numberOfFruitsAdded
 		print("Number of Oranges: " + str(numberOfOranges))
+	elif fruitName == "pear":
+		numberOfPears += numberOfFruitsAdded
+		print("Number of Pears: " + str(numberOfPears))
+
+static func removeFruits(numberOfFruitsRemoved: int, fruitName: String) -> void:
+	numberOfFruits += numberOfFruitsRemoved
+	print("Number Of Fruits: " + str(numberOfFruits))
+	if fruitName == "apple":
+		numberOfApples -= numberOfFruitsRemoved
+		print("Number of Apples: " + str(numberOfApples))
+	elif fruitName == "orange":
+		numberOfOranges -= numberOfFruitsRemoved
+		print("Number of Oranges: " + str(numberOfOranges))
+	elif fruitName == "pear":
+		numberOfPears -= numberOfFruitsRemoved
+		print("Number of Pears: " + str(numberOfPears))
+
+static func fruitWorth(fruitName: String) -> int:
+	return fruitValues[fruitName]
