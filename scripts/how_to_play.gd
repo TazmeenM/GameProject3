6547496b793a12cf912d0f0327bf5a27de7cda08
TazmeenM <tazmeen.mohsin@gmail.com>
2025-08-29 19:16:01 -0400

    Created a How To Play Scene
    Created a How To Play scene, including a series of images explaining how to play that the player can click through

--- scripts/how_to_play.gd
+++ scripts/how_to_play.gd
@@ -1,14 +1,38 @@
 extends Node2D
 
+@onready var instruction_image: Sprite2D = $InstructionImage
+@onready var next_button: Button = $NextButton
+@onready var previous_button: Button = $PreviousButton
+
 var imageNumber = 0
-var imageDirectoryRoot = "res://assets/sprites/"
-var images = []
+var imageDirectoryRoot = "res://assets/sprites/howToPlay"
+var images = ["1", "2", "3", "4", "5", "6", "7", "8"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	loadImage()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+func _on_next_button_pressed() -> void:
+	if imageNumber < images.size() - 1:
+		imageNumber += 1
+		loadImage()
+		if imageNumber == images.size() - 1:
+			next_button.text = "Finish"
+	else:
+		next_button.text = "Next"
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+
+
+func _on_previous_button_pressed() -> void:
+	if imageNumber > 0:
+		imageNumber -= 1
+		loadImage()
+
+func loadImage() -> void:
+	instruction_image.texture = load(imageDirectoryRoot + images[imageNumber] + ".png")
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -15,3 +15,7 @@ func _on_play_button_pressed() -> void:
 	HealthTimer.timer.start()
 	Astronaut.resetGame = false
 	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+
+
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
